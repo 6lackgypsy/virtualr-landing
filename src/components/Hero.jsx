96cffd4558aa98d81061a9ed6ef0524e2cbea7a6
6lackgypsy <alfreddennis26.adt90@gmail.com>
2025-04-02 02:01:25 +0100

    Replaced video files with video addresses

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,3 @@
-import video1 from "../assets/video1.mp4"
-import video2 from "../assets/video2.mp4"
-
 const Hero = () => {
   return (
     <div className="flex flex-col items-center mt-6 lg:mt-20">
@@ -33,7 +30,10 @@ const Hero = () => {
           muted
           className="rounded-lg  w-3/4 md:w-1/2 border border-orange-600 shadow-orange-400 mx-2 my-4 "
         >
-          <source src={video1} type="video/mp4" />
+          <source
+            src="https://www.shutterstock.com/shutterstock/videos/1111379565/preview/stock-footage-back-view-of-a-young-engineer-working-on-computer-in-a-technological-office-environment-male.webm"
+            type="video/mp4"
+          />
           Your browser does not support the video tag
         </video>
         <video
@@ -42,7 +42,10 @@ const Hero = () => {
           muted
           className="rounded-lg  w-3/4 md:w-1/2 border border-orange-600 shadow-orange-400 mx-2 my-4 "
         >
-          <source src={video2} type="video/mp4" />
+          <source
+            src="https://www.shutterstock.com/shutterstock/videos/3520151293/preview/stock-footage-exploring-virtual-world-with-vr-headset-in-an-industrial-space-immersive-innovation.webm"
+            type="video/mp4"
+          />
           Your browser does not support the video tag
         </video>
       </div>
